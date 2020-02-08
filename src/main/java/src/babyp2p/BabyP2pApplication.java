3e0811e8d8d1057eb7a1da60ddae12e8dcb6632e
xhdouzi <1289633872@qq.com>
2020-02-08 10:43:49 +0800

    ccc

---
+++ src/main/java/src/babyp2p/BabyP2pApplication.java
@@ -0,0 +1,13 @@
+package src.babyp2p;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BabyP2pApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BabyP2pApplication.class, args);
+    }
+
+}
---
+++ src/test/java/src/babyp2p/BabyP2pApplicationTests.java
@@ -0,0 +1,13 @@
+package src.babyp2p;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BabyP2pApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
